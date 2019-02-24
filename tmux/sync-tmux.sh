4e8dab7b56649a2669646d09f31ea5c7228c38ad
Dank <dan.e.karlsson@jpl.nasa.gov>
2019-02-23 17:45:54 -0800

    Organized the project.

--- tmux/sync-tmux.sh
+++ tmux/sync-tmux.sh
@@ -2,11 +2,11 @@
 
 if [[ "$1" = "local" ]]; then
     cp ~/.tmux.conf ~/.tmux.conf.bak
-    cp ~/git/env-config/tmux.conf ~/.tmux.conf
+    cp ~/git/env-config/tmux/tmux.conf ~/.tmux.conf
 else
-    cp ~/.tmux.conf ~/git/env-config/tmux.conf
+    cp ~/.tmux.conf ~/git/env-config/tmux/tmux.conf
     cd ~/git/env-config/
-    git add tmux.conf
+    git add tmux/tmux.conf
     commit_message="Updated tmux.conf on `whoami`@`hostname`"
     git commit -m "$commit_message"
 fi

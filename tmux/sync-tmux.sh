46063c2b79442b92aecd3a489a04bf5e49928969
Erik Karlsson <erikdkarlsson8@gmail.com>
2019-02-23 19:17:23 -0800

    Need be specific about remote and local

--- tmux/sync-tmux.sh
+++ tmux/sync-tmux.sh
@@ -3,7 +3,8 @@
 if [[ "$1" = "local" ]]; then
     cp ~/.tmux.conf ~/.tmux.conf.bak
     cp ~/git/env-config/tmux/tmux.conf ~/.tmux.conf
-else
+
+elif [[ "$1" = "remote" ]]; then
     cp ~/.tmux.conf ~/git/env-config/tmux/tmux.conf
     cd ~/git/env-config/
     if [[ "$1" = "commit" ]]; then
@@ -11,5 +12,10 @@ else
         commit_message="Updated tmux.conf on `whoami`@`hostname`"
         git commit -m "$commit_message"
     fi
+else
+    echo
+    echo "Usage: sync-tmux.sh < local | remote >"
+    echo "Specify local or remote"
+    echo
 fi
     
